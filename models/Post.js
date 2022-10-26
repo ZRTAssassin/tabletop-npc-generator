2d527678bad4a987de9a71524f2bbe520ca0edd5
Ryan Warren <ryanwarren678@gmail.com>
2022-10-26 11:35:49 -0500

    Initial commit

---
+++ models/Post.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  caption: {
+    type: String,
+    required: true,
+  },
+  deck: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Post", PostSchema);
