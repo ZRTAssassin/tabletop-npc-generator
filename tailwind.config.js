ded06ba1bb4544da6bee55bc8204698bae409c5d
Ryan Warren <ryanwarren678@gmail.com>
2022-10-27 13:47:07 -0500

    Fixing daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/*.{html,js,css,ejs}"],
+  content: ["./views/*/*.{html,css,js,ejs}", "./views/*.{html,css,js,ejs}"],
   theme: {
     extend: {},
   },
+  // add daisyUI plugin
   plugins: [require("daisyui")],
+
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
 };
